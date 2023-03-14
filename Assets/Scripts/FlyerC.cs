e09985d3afb3de23a7ef0fd09ad493c05448d43f
brycebohling <96216764+brycebohling@users.noreply.github.com>
2023-03-14 15:05:40 -0700

    working flyer

--- Assets/Scripts/FlyerC.cs
+++ Assets/Scripts/FlyerC.cs
@@ -13,6 +13,27 @@ public class FlyerC : MonoBehaviour
     // Health
     int currentHealth;
     [SerializeField] int health;
+
+    // Attack
+
+    float attackCountdown;
+    [SerializeField] float attackTimer;
+    [SerializeField] float attackSpeed;
+    bool attacking;
+    bool divedIn;
+    bool divedOut;
+    Vector3 originalPos;
+    Vector3 attackPlayerPos;
+
+    // Movement 
+    
+    Vector2 targetLocationX;
+    Vector2 targetLocationY;
+    float distanceX;
+    float distanceY;
+    float distance;
+    [SerializeField] float minDistanceX;
+
     
     void Start()
     {
@@ -24,8 +45,61 @@ public class FlyerC : MonoBehaviour
     
     void Update()
     {
-        transform.position =  Vector2.Lerp(transform.position, GameManager.gameManager.player.position, speed * Time.deltaTime);
+
+        targetLocationX = new Vector2(GameManager.gameManager.player.position.x, transform.position.y);
+        targetLocationY = new Vector2(transform.position.x, GameManager.gameManager.player.position.y);
+        distance = Vector2.Distance(transform.position, GameManager.gameManager.player.position);
+        distanceX = Vector2.Distance(transform.position, targetLocationX);
+        distanceY = Vector2.Distance(transform.position, targetLocationY);
+
+        if (attackCountdown > 0)
+        {
+            attackCountdown -= Time.deltaTime;
+        }
+
+        if (attacking)
+        {   
+            if (!divedIn)
+            {   
+                transform.position =  Vector2.Lerp(transform.position, attackPlayerPos, attackSpeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, attackPlayerPos) < 1)
+                {
+                    divedIn = true;
+                }
+
+            } else if (!divedOut)
+            {
+                transform.position =  Vector2.Lerp(transform.position, originalPos, attackSpeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, originalPos) < 1f)
+                {
+                    divedOut = true;
+                }
+            }
+            return;
+        }
+                
+        if (distanceX > minDistanceX)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, GameManager.gameManager.player.position, speed * Time.deltaTime);
+
+        } else if (attackCountdown <= 0 && distanceY < 5)
+        {
+            attacking = true;
+            attackCountdown = attackTimer;
+            StartCoroutine(AttackPlayer());   
+        }
     }
 
-    
+    private IEnumerator AttackPlayer()
+    {
+        originalPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        attackPlayerPos = new Vector3(GameManager.gameManager.player.position.x, GameManager.gameManager.player.position.y, GameManager.gameManager.player.position.z);
+        divedIn = false;
+        divedOut = false;
+
+        yield return new WaitUntil(() => divedIn);
+        yield return new WaitUntil(() => divedOut);
+
+        attacking = false;
+    }
 }
