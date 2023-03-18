e74b3199cd9dbb75c0fcf2be02200feed7fecd1c
brycebohling <96216764+brycebohling@users.noreply.github.com>
2023-03-18 00:24:15 -0700

    working on UI

---
+++ Assets/Scripts/SceneC.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneC
+{
+    public static void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public static void Restart()
+    {
+        LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/UIC.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIC : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneC.LoadScene(1);
+    }
+
+    public void Restart()
+    {
+        SceneC.Restart();
+    }
+}
