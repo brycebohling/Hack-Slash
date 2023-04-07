65a61d8cbb074518a5280718b912d74ec274016f
brycebohling <96216764+brycebohling@users.noreply.github.com>
2023-04-06 23:40:10 -0700

    now the wave spawn doesnt go over its value

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -7,7 +7,7 @@ public class WaveSpawner : MonoBehaviour
     [System.Serializable]
     public struct EnemyType
     {
-        public GameObject prefab; // The enemy prefab
+        public GameObject prefab;
         public float value; 
     }
 
@@ -17,56 +17,39 @@ public class WaveSpawner : MonoBehaviour
     }
 
     public SpawnPoints[] spawnPoints;
-    public EnemyType[] enemyTypes; // Array of enemy types to choose from
-    [SerializeField] float waveValue; // The total value of the wave
-    [SerializeField] float valueIncreasePerWave; // The value increase for each wave
-    public float timeBetweenSpawns; // The time between each enemy spawn
-    // public Transform spawnPoint; // The point where enemies spawn
+    public EnemyType[] enemyTypes; 
+    [SerializeField] float waveValue;
+    [SerializeField] float valueIncreasePerWave; 
+    public float timeBetweenSpawns;
     [SerializeField] float timeBetweenWaves;
 
     float waveTimer;
-    private float currentWaveValue; // The current value of the wave
-    private float spawnTimer; // The timer for enemy spawns
-    Transform randomSpawnPoint;
+    private float currentWaveValue;
+    private float spawnTimer;
 
     void Start()
     {
-        // Initialize the current wave value
         currentWaveValue = waveValue;
     }
 
     void Update()
     {
-        
-
-        // Check if it's time to spawn a new enemy
         if (spawnTimer <= 0f && currentWaveValue > 0f && waveTimer <= 0)
         {
-            // Choose a random enemy type from the array based on its value
-            float totalValue = 0f;
-            foreach (EnemyType enemyType in enemyTypes)
+            
+            int enemyIndex = Random.Range(0, enemyTypes.Length);
+
+            while(enemyTypes[enemyIndex].value > currentWaveValue) 
             {
-                totalValue += enemyType.value;
-            }
-            float randomValue = Random.Range(0f, totalValue);
-            int enemyIndex = 0;
-            foreach (EnemyType enemyType in enemyTypes)
-            {
-                if (randomValue <= enemyType.value)
-                {
-                    break;
-                }
-                randomValue -= enemyType.value;
-                enemyIndex++;
+                enemyIndex = Random.Range(0, enemyTypes.Length);
             }
 
-            randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].spawnPoint;
+            Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].spawnPoint;
+
             GameObject newEnemy = Instantiate(enemyTypes[enemyIndex].prefab, randomSpawnPoint.position, randomSpawnPoint.rotation);
 
-            // Subtract the value cost of the enemy from the current wave value
             currentWaveValue -= enemyTypes[enemyIndex].value;
 
-            // Reset the spawn timer
             spawnTimer = timeBetweenSpawns;
 
             Debug.Log(currentWaveValue);
@@ -83,13 +66,10 @@ public class WaveSpawner : MonoBehaviour
             }
         }
 
-        // Check if the wave is over
         if (currentWaveValue <= 0f)
         {
-            // Increase the wave value for the next wave
             waveValue += valueIncreasePerWave;
 
-            // Reset the current wave value and spawn timer
             currentWaveValue = waveValue;
             spawnTimer = 0f;
 
