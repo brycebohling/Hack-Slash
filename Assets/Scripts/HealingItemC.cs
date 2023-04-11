97f21e410c7e41af9a539fcdf5041ac307be4aad
brycebohling <96216764+brycebohling@users.noreply.github.com>
2023-04-11 16:52:19 -0700

    working apple shine

--- Assets/Scripts/HealingItemC.cs
+++ Assets/Scripts/HealingItemC.cs
@@ -8,10 +8,17 @@ public class HealingItemC : MonoBehaviour
     [SerializeField] Vector2 hitBoxSize;
     [SerializeField] int healingAmount;
     bool isTouchingPlayer;
+    Animator anim;
+    string _currentState;
+    [SerializeField] float timeBetweenShines;
+    float shineTimer;
+    string NORMAL = "static";
+    string SHINE = "shine";
 
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        shineTimer = timeBetweenShines;
     }
 
     void Update()
@@ -22,6 +29,42 @@ public class HealingItemC : MonoBehaviour
         {
             GameManager.gameManager.HealPlayer(healingAmount);
             Destroy(gameObject);
+        }
+
+        if (shineTimer > 0f)
+        {
+            shineTimer -= Time.deltaTime;
+            if (!IsAnimationPlaying(anim, SHINE))
+            {
+                ChangeAnimationState(NORMAL);
+            }
+
+        } else 
+        {
+            ChangeAnimationState(SHINE);
+            shineTimer = timeBetweenShines;
+        }
+    }
+
+    private void ChangeAnimationState(string newState)
+    {
+        if (newState == _currentState)
+        {
+            return;
+        }
+
+        anim.Play(newState);
+        _currentState = newState;
+    }
+
+    private bool IsAnimationPlaying(Animator animator, string stateName)
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName(stateName) && animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+        {
+            return true;
+        } else
+        {
+            return false;
         }
     }
 
